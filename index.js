18ac84a61f07fcf148e7bf15871271b4661e37c0
Dmitry Minkovsky <dminkovsky@gmail.com>
2015-10-06 17:34:43 -0400

    Fixed bug

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ module.exports = function(content) {
     if (this.cacheable) { this.cacheable(); }
     // Wrangle the result of `css-loader` so that it can be `eval()`ed
     content = content.split('\n');
-    content[0] = content[0].replace('./node_modules/', '');
+    content[0] = 'exports = module.exports = require("css-loader/lib/css-base.js")();';
     content = content.join('\n');
     eval(content);
     return "module.exports = " + JSON.stringify(cssToRadium(exports[0][1])) + ";";
