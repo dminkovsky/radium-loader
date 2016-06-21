fe610c9dd3d5878764e0c1e27a91a0fb269a0f58
Luigi Poole <luigipoole@outlook.com>
2016-06-21 11:33:08 -0700

    alter project directory structure

--- test.js
+++
@@ -1,8 +0,0 @@
-// Loader for `/\.css$/` files configured in  webpack.config.js 
-var radiumObject = require('./test.css');
-
-function assertIsObject(obj) {
-    if (typeof obj !== 'object') { throw new Error('Expected object'); }
-}
-
-assertIsObject(radiumObject);
